5bd05430c907e1da3171be1ff765c06d33a0d68b
shubaivan <shuba.ivan@yandex.ru>
2014-12-08 15:52:15 +0200

    add PHPUnit test from Entity/Blog.php and twig filter createdAgo

---
+++ src/Blogger/BlogBundle/Tests/Entity/BlogTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivan
+ * Date: 08.12.14
+ * Time: 15:27
+ */
+namespace Blogger\BlogBundle\Tests\Entity;
+
+use Blogger\BlogBundle\Entity\Blog;
+
+class BlogTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testSlugify()
+    {
+        $blog = new Blog();
+
+        $this->assertEquals('hello-world', $blog->slugify('Hello World'));
+        $this->assertEquals('a-day-with-symfony2', $blog->slugify('A Day With Symfony2'));
+        $this->assertEquals('hello-world', $blog->slugify('Hello    world'));
+        $this->assertEquals('symblog', $blog->slugify('symblog '));
+        $this->assertEquals('symblog', $blog->slugify(' symblog'));
+    }
+    public function testSetSlug()
+    {
+        $blog = new Blog();
+
+        $blog->setSlug('Symfony2 Blog');
+        $this->assertEquals('symfony2-blog', $blog->getSlug());
+    }
+
+    public function testSetTitle()
+    {
+        $blog = new Blog();
+
+        $blog->setTitle('Hello World');
+        $this->assertEquals('hello-world', $blog->getSlug());
+    }
+
+}
---
+++ src/Blogger/BlogBundle/Tests/Twig/Extensions/BloggerBlogExtensionTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivan
+ * Date: 08.12.14
+ * Time: 15:38
+ */
+namespace Blogger\BlogBundle\Tests\Twig\Extensions;
+
+use Blogger\BlogBundle\Twig\Extensions\BloggerBlogExtension;
+
+class BloggerBlogExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreatedAgo()
+    {
+        $blog = new BloggerBlogExtension();
+
+        $this->assertEquals("0 seconds ago", $blog->createdAgo(new \DateTime()));
+        $this->assertEquals("34 seconds ago", $blog->createdAgo($this->getDateTime(-34)));
+        $this->assertEquals("1 minute ago", $blog->createdAgo($this->getDateTime(-60)));
+        $this->assertEquals("2 minutes ago", $blog->createdAgo($this->getDateTime(-120)));
+        $this->assertEquals("1 hour ago", $blog->createdAgo($this->getDateTime(-3600)));
+        $this->assertEquals("1 hour ago", $blog->createdAgo($this->getDateTime(-3601)));
+        $this->assertEquals("2 hours ago", $blog->createdAgo($this->getDateTime(-7200)));
+
+        // Cannot create time in the future
+        $this->setExpectedException('\InvalidArgumentException');
+        $blog->createdAgo($this->getDateTime(60));
+    }
+
+    protected function getDateTime($delta)
+    {
+        return new \DateTime(date("Y-m-d H:i:s", time()+$delta));
+    }
+}
